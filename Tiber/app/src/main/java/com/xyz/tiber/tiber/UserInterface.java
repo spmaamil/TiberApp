f4d4bcea291499e7c2676548f880eabdb158f5a7
Jett Kaspar <spmaamil@gmail.com>
2018-10-19 19:38:29 -0500

    First Commit

---
+++ Tiber/app/src/main/java/com/xyz/tiber/tiber/UserInterface.java
@@ -0,0 +1,16 @@
+package com.xyz.tiber.tiber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserInterface extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_interface);
+
+    }
+}
